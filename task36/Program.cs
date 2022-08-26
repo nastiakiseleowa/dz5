65a57f5906ee766561911d118785817ee24ea06e
Nastia Kiseleowa <nastia.kiseleowa@yandex.ru>
2022-08-26 19:28:30 +0300

    task36

---
+++ task36/Program.cs
@@ -0,0 +1,34 @@
+﻿void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, 100);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[4];
+
+FillArray(array);
+PrintArray(array);
+
+int n = array.Length;
+int sum = 0;
+for (int i = 1; i < array.Length; i += 2)
+{
+    sum = sum += array[i];
+}
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {sum}");
