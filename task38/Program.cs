6fe3cf4cef0d4b099d0fd52d4d8f2c42e088cff1
Nastia Kiseleowa <nastia.kiseleowa@yandex.ru>
2022-08-26 19:28:48 +0300

    task38

---
+++ task38/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] array = { 6, 47, 63, 87, 51 };
+
+int max = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (max < array[i])
+    {
+        max = array[i];
+    }
+}
+Console.WriteLine($"Вывести максимальное значение: {max}");
+
+int min = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (min > array[i])
+    {
+        min = i;
+    }
+}
+Console.WriteLine($"Вывести минимальное значение: {min}");
+
+int result = max - min;
+Console.WriteLine($"Вывести результат: {result}");
+
+
+
